2de8dc8a81f3b5b77c686f8cbd8177be2c1f48d1
John <johndoemisc@tutamail.com>
2020-11-14 23:28:13 +0300

    Extension technically complete. Some refactoring/QoL improvements left

--- group.js
+++ group.js
@@ -7,28 +7,35 @@ console.log('extension code start')
 function onGot(item) {
   let result = [];
   console.log(item);
-  const header = (document.querySelector('.HeaderNav')) ? document.querySelector('.HeaderNav') : document.querySelector('.UnauthorizedHeader');
   console.log(header);
   console.log(window.location.hostname);
   let hostname = window.location.hostname;
-  let div = document.createElement("div");
-  console.log(div);
-  div.classList.add("dropdown-menu");
-  header.appendChild(div);
+  let menu = document.createElement("div");
+  console.log(menu);
+  menu.classList.add("dropdown-menu");
+  // refactor next 2 lines - put document.querySelector inside if-block;
+  const header = (document.querySelector('.HeaderNav')) ? document.querySelector('.HeaderNav')
+  : document.body;
+  if (document.querySelector('HeaderNav')) {
+    header.appendChild(menu);
+  } else {
+    header.prepend(menu);
+  }
+  let btn = document.createElement("button");
+  btn.classList.add("dropdown-btn");
+  menu.appendChild(btn);
+  let content = document.createElement("div");
+  content.classList.add("dropdown-content");
+  menu.appendChild(content);
   for (let i = 0; i < item.vk.length; i++) {
     console.log('loop')
     let href = item.vk[i];
     let link = document.createElement("a");
     link.appendChild(document.createTextNode(item.vk[i]));
     link.href = href;
-    div.appendChild(link);
+    content.appendChild(link);
   }
 }
 
 let getting = browser.storage.local.get("vk");
 getting.then(onGot, onError);
-
-
-let styleURL = browser.runtime.getURL("dropdown.css");
-let inserting = browser.tabs.insertCSS({file: styleURL});
-inserting.then(null, onError);
